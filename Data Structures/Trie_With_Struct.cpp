76cf23f0fb09536c808853f5933343433934632d
Beso Managadze <113914065+bmana21@users.noreply.github.com>
2024-10-24 16:43:13 +0400

    added trie implementation with struct

---
+++ Data Structures/Trie_With_Struct.cpp
@@ -0,0 +1,37 @@
+struct TrieNode {
+    TrieNode *next[26]{};  // We Only Have Lower Case Latin Letter For This Implementation {a..z}
+    int data;  // Can Be Any Type
+
+    TrieNode() {
+        for (auto & i : next) {
+            i = nullptr;
+        }
+        data = -1;
+    }
+
+    // Add String In The Trie With Value d (example: trie.add("HelloWorld", 12); )
+    void add(const string &s, int d, int pos = 0) {
+        if (pos == s.size()) {
+            data = d;
+            return;
+        }
+        int p = s[pos] - 'a';
+        if (next[p] == nullptr)
+            next[p] = new TrieNode();
+
+        next[p]->add(s, d, pos + 1);
+
+    }
+
+    // Add String In The Trie With Value d (example: int data = trie.get("HelloWorld"); )
+    int get(const string &s, int pos = 0) {
+        if (s.size() == pos) {
+            return data;
+        }
+        int p = s[pos] - 'a';
+        if (next[p] == nullptr)
+            return -1;
+        else return next[p]->get(s, pos + 1);
+    }
+
+};
